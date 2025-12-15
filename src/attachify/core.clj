21a2b3266ac30472343d1fa8ab24e262058ea86a
riwepo <riwepo.au@gmail.com>
2025-12-15 14:30:44 +1100

    imported cheshire and http client trying to read inbox

--- src/attachify/core.clj
+++ src/attachify/core.clj
@@ -1,6 +1,18 @@
 (ns attachify.core
-  (:require [yogthos.config :as config]))
+  (:require [config.core :refer [env]]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
-(def app-config (config/load-config))
 
-(println "API Token:" (:api-token app-config))
+
+(def myEnv (edn/read-string (slurp "resources/config.edn")))
+
+(comment
+  (io/resource "resources/config.edn")
+  (slurp (io/resource "config.edn"))
+  (slurp  "resources/config.edn")
+  (println myEnv)
+  (println "API Token:" (:fastmail_api_token myEnv))
+  nil)
+
+
--- src/attachify/fastmail.clj
+++ src/attachify/fastmail.clj
@@ -1,1 +1,40 @@
-(ns attachify.fastmail)
+(ns attachify.fastmail
+  (:require [clj-http.client :as http]
+            [cheshire.core :as json]))
+
+(def my-api-url "https://api.fastmail.com/jmap/api/")
+(def my-access-token "fmu1-5c164056-5db4226acdc2c14ad1008fecc8082146-0-63118a547fec2b239a083d31bb977231")
+
+(defn fetch-inbox-emails
+  [api-url access-token]
+  (let [
+        headers {"Authorization" (str "Bearer " access-token)
+                 "Content-Type"  "application/json; charset=utf-8"}
+        ;; Step 1: Query email IDs in Inbox
+        query-body {:using ["urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"]
+                    :methodCalls [
+                                  ["Email/query"
+                                   {
+                                    :accountId "accountId",
+                                    :filter   { :inMailbox "accountId" },
+                                    :sort     [ { :property "receivedAt", :isAscending false } ],
+                                    :limit    10}
+                                   ,
+                                   "a"]]}
+
+
+        response (http/post api-url
+                            {:headers headers
+                             :body (json/encode query-body)
+                             :as :auto})]
+
+    (println headers)
+    (println query-body)
+    (println "Response status:" (:status response))
+    (println "Response headers:" (:headers response))))
+
+
+(comment
+    (fetch-inbox-emails my-api-url my-access-token)
+  nil)
+
