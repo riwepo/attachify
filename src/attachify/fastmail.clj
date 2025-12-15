7b87ff864d1b20a272b5fae08a555cae764042c3
riwepo <riwepo.au@gmail.com>
2025-12-15 14:55:01 +1100

    working on get session

--- src/attachify/fastmail.clj
+++ src/attachify/fastmail.clj
@@ -2,8 +2,20 @@
   (:require [clj-http.client :as http]
             [cheshire.core :as json]))
 
+(def hostname "api.fastmail.com")                           ;
+(def username "attachify")
+(def authUrl (str "https://" hostname "/.well-known/jmap"))
+
+
 (def my-api-url "https://api.fastmail.com/jmap/api/")
 (def my-access-token "fmu1-5c164056-5db4226acdc2c14ad1008fecc8082146-0-63118a547fec2b239a083d31bb977231")
+
+(defn get-session
+  [auth-url access-token]
+  (let [headers {"Authorization" (str "Bearer " access-token)
+                 "Content-Type"  "application/json; charset=utf-8"}
+        response (http/get auth-url {:headers headers :as :json})]
+    (:body response)))
 
 (defn fetch-inbox-emails
   [api-url access-token]
