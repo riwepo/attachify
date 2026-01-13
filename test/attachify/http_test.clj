18c5897b617a4d96d4c8e118775ad0db11ee0c41
riwepo <riwepo.coding@gmail.com>
2026-01-14 09:18:33 +1100

    test post functions

--- test/attachify/http_test.clj
+++ test/attachify/http_test.clj
@@ -5,7 +5,7 @@
             [attachify.config :refer :all]
             [attachify.http :refer :all]))
 
-(t/deftest get2-bad-url-test
+(t/deftest get2-fail-bad-url-test
   (let [bad-url "some-dodgy-url"
         config (load-config)
         token (:email-api-token config)
@@ -17,7 +17,7 @@
         (t/is (failure? result))
         (t/is (= "get2 failed some-dodgy-url 404" (:error result)))))))
 
-(t/deftest get2-bad-token-test
+(t/deftest get2-fail-bad-token-test
   (let [config (load-config)
         url (get-email-auth-url config)
         token "some-dodgy-token"
@@ -40,4 +40,63 @@
       (let [result (get2 url token)]
         (t/is (success? result))
         (t/is (= "the body" (:value result)))))))
+
+(t/deftest post2-bad-url-test
+  (let [bad-url "some-dodgy-url"
+        config (load-config)
+        token (:email-api-token config)
+        content "content"
+        content-type "text/plain"
+        mock-post (fn [_url _opts]
+                    {:status 404
+                     :body "Not Found"})]
+    (with-redefs [http/post mock-post]
+      (let [result (post2 bad-url token content content-type)]
+        (t/is (failure? result))
+        (t/is (= "post2 failed 404 some-dodgy-url" (:error result)))))))
+
+(def api-url "https://api.fastmail.com/jmap/api/")
+
+(t/deftest post2-bad-token-test
+  (let [url api-url
+        token "some-dodgy-token"
+        content "content"
+        content-type "text/plain"
+        mock-post (fn [_url _opts]
+                    {:status 401
+                     :body "Not Authorized"})]
+    (with-redefs [http/post mock-post]
+      (let [result (post2 url token content content-type)]
+        (t/is (failure? result))
+        (t/is (= (str "post2 failed 401 " api-url) (:error result)))))))
+
+(t/deftest post2-bad-request-test
+  (let [config (load-config)
+        url api-url
+        token (:email-api-token config)
+        content nil
+        content-type "text/plain"
+        mock-post (fn [_url _opts]
+                    {:status 400
+                     :body "Bad Request"})]
+    (with-redefs [http/post mock-post]
+      (let [result (post2 url token content content-type)]
+        (t/is (failure? result))
+        (t/is (= (str "post2 failed 400 " api-url) (:error result)))))))
+
+(t/deftest post2-success-test
+  (let [config (load-config)
+        url api-url
+        token (:email-api-token config)
+        content "content"
+        content-type "text/plain"
+        mock-post (fn [_url _opts]
+                    {:status 200
+                     :body "the body"})]
+    (with-redefs [http/post mock-post]
+      (let [result (post2 url token content content-type)]
+        (t/is (success? result))
+        (t/is (= "the body" (:value result)))))))
+
+
 
