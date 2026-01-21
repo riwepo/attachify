1c728b2b375db763ae49740c8de61c4b36cdd015
riwepo <riwepo.coding@gmail.com>
2026-01-21 12:34:12 +1100

    return value

--- src/attachify/core.clj
+++ src/attachify/core.clj
@@ -101,7 +101,7 @@
                         (loop [ids all-email-ids
                                success-count 0]
                           (if (empty? ids)
-                            {:value success-count}
+                            (log-and-success "process-emails succeeded")
                             (let [email-id (first ids)
                                   process-email-result (process-email config session sender-id mailbox-info email-id)]
                               (if (failure? process-email-result)
