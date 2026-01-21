897eeaa41953b65b404e12e5c5ca07c3de95af3c
riwepo <riwepo.coding@gmail.com>
2026-01-22 09:03:06 +1100

    missing if block

--- src/attachify/core.clj
+++ src/attachify/core.clj
@@ -35,39 +35,39 @@
               (let [resend-address (build-resend-address config resend-username)
                     blobs-info (fm/get-blobs-info email)
                     download-blobs-result (fm/download-blobs session blobs-info)]
-                (failure? download-blobs-result)
-                (log-and-failure "process-email failed Step 4" (:error download-blobs-result))
-                (let [blobs (:value download-blobs-result)
-                      attachment-blobs (filter #(= (:role %) :attachment) blobs)
-                      upload-attachments-result (fm/upload-attachments session attachment-blobs)]
-                  (if (failure? upload-attachments-result)
-                    (log-and-failure "process-email failed Step 5" (:error upload-attachments-result))
-                    (let [attachment-info (:value upload-attachments-result)
-                          from-address (:from-address config)
-                          draft-email-object (fm/build-draft-email
-                                               blobs
-                                               attachment-info
-                                               from-address
-                                               resend-address
-                                               (fm/get-mailbox-id-by-role mailbox-info "drafts")
-                                               (:subject email))
-                          create-draft-result (fm/create-draft-email session draft-email-object)]
-                      (if (failure? create-draft-result)
-                        (log-and-failure "process-email failed Step 6" (:error create-draft-result))
-                        (let [draft-email-id (:value create-draft-result)
-                              submit-result (fm/submit-email session draft-email-id sender-id)]
-                          (if (failure? submit-result)
-                            (log-and-failure "process-email failed Step 7" (:error submit-result))
-                            (let [sent-mailbox-id (fm/get-mailbox-id-by-role mailbox-info "sent")
-                                  move-email-to-sent-result (fm/move-email-to-mailbox session draft-email-id sent-mailbox-id)]
-                              (if (failure? move-email-to-sent-result)
-                                (log-and-failure "process-email failed Step 8" (:error move-email-to-sent-result))
-                                (let [processed-mailbox-id (fm/get-mailbox-id-by-name mailbox-info "Processed")
-                                      move-email-to-processed-result (fm/move-email-to-mailbox session email-id processed-mailbox-id)]
-                                  (if (failure? move-email-to-processed-result)
-                                    (log-and-failure "process-email failed Step 9" (:error move-email-to-processed-result))
-                                    ;; All steps succeeded
-                                    (log-and-success nil "email successfully processed")))))))))))))))))))
+                (if (failure? download-blobs-result)
+                     (log-and-failure "process-email failed Step 4" (:error download-blobs-result))
+                     (let [blobs (:value download-blobs-result)
+                           attachment-blobs (filter #(= (:role %) :attachment) blobs)
+                           upload-attachments-result (fm/upload-attachments session attachment-blobs)]
+                       (if (failure? upload-attachments-result)
+                         (log-and-failure "process-email failed Step 5" (:error upload-attachments-result))
+                         (let [attachment-info (:value upload-attachments-result)
+                               from-address (:from-address config)
+                               draft-email-object (fm/build-draft-email
+                                                    blobs
+                                                    attachment-info
+                                                    from-address
+                                                    resend-address
+                                                    (fm/get-mailbox-id-by-role mailbox-info "drafts")
+                                                    (:subject email))
+                               create-draft-result (fm/create-draft-email session draft-email-object)]
+                           (if (failure? create-draft-result)
+                             (log-and-failure "process-email failed Step 6" (:error create-draft-result))
+                             (let [draft-email-id (:value create-draft-result)
+                                   submit-result (fm/submit-email session draft-email-id sender-id)]
+                               (if (failure? submit-result)
+                                 (log-and-failure "process-email failed Step 7" (:error submit-result))
+                                 (let [sent-mailbox-id (fm/get-mailbox-id-by-role mailbox-info "sent")
+                                       move-email-to-sent-result (fm/move-email-to-mailbox session draft-email-id sent-mailbox-id)]
+                                   (if (failure? move-email-to-sent-result)
+                                     (log-and-failure "process-email failed Step 8" (:error move-email-to-sent-result))
+                                     (let [processed-mailbox-id (fm/get-mailbox-id-by-name mailbox-info "Processed")
+                                           move-email-to-processed-result (fm/move-email-to-mailbox session email-id processed-mailbox-id)]
+                                       (if (failure? move-email-to-processed-result)
+                                         (log-and-failure "process-email failed Step 9" (:error move-email-to-processed-result))
+                                         ;; All steps succeeded
+                                         (log-and-success nil "email successfully processed"))))))))))))))))))))
 
 
 
