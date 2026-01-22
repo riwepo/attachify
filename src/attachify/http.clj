b4e6d1558b3237e3c1db07c17773a5a42fa42e0e
riwepo <riwepo.coding@gmail.com>
2026-01-22 13:48:07 +1100

    get requires data type parameter

--- src/attachify/http.clj
+++ src/attachify/http.clj
@@ -1,13 +1,13 @@
 (ns attachify.http
   (:require [clj-http.client :as http]
-            [cheshire.core :as json]
             [attachify.result :refer [success]]
             [attachify.result-log :refer [log-and-success log-and-failure]]))
 
-(defn get2 [url api-token]
+(defn get2 [url api-token data-type]
+  ;; possible data types :text :json :byte-array
   (let [headers {"Authorization" (str "Bearer " api-token)}]
     (try
-      (let [response (http/get url {:headers headers :as :auto})
+      (let [response (http/get url {:headers headers :as data-type})
             status (:status response)]
         (if (= status 200)
           (do
@@ -16,8 +16,6 @@
           (log-and-failure "get2 failed" url status)))
       (catch Exception e
         (log-and-failure "get2 failed" url (.getMessage e))))))
-
-;"application/json; charset=utf-8"
 
 (defn post2 [url api-token content content-type]
   ;; if content-type is json, the content must be json encoded
